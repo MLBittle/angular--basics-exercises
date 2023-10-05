5159cd490a95444d460e684e786aad65162e7cd5
Mary Linda Bittle <140030405+MLBittle@users.noreply.github.com>
2023-10-05 11:14:19 -0500

    continuing on with adding recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe'), "This is a simple test", 'https://images.freeimages.com/images/large-previews/e7c/recipe-1538714.jpg'
+];
 
+constructor() { }
+
+ngOnInit() {
+
+}
 }
---
+++ src/app/recipes/recipe..model.ts
@@ -0,0 +1,13 @@
+
+export class Recipe {
+  public title: string;
+  public description: string;
+  public imagePath: string;
+
+
+constructor Recipe (title: string, desc: string, imagePath: string)
+ this.title  = title;
+ this.description = desc;
+ this.imagePath = imagePath;
+
+}
